736662144dfe84b675c58f95b17779296b5040ee
wendyzura <wendyzura@hotmail.com>
2025-05-13 13:32:11 +0000

    1

--- app.py
+++ app.py
@@ -8,6 +8,19 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/gemini",methods=["GET","POST"])
+def gemini():
+    return(render_template("gemini.html"))
+
+@app.route("/gemini_reply",methods=["GET","POST"])
+def gemini_reply():
+    q = request.form.get("q")
+    print(q)
+    #gemini
+    r=q
+
+    return(render_template("gemini_reply.html",r=r))
+
 if __name__ == "__main__":
     app.run()
     
