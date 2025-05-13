73354cea7206394856ef30f26d98cb16997c31c8
wendyzura <wendyzura@hotmail.com>
2025-05-13 13:50:54 +0000

    1

--- app.py
+++ app.py
@@ -1,26 +1,20 @@
 #gemini
-
-from flask import Flask, request,render_template
-
+from flask import Flask,request,render_template
+import google.generativeai as genai
+genai.configure(api_key="AIzaSyB9szziVsPc8wEmYJoDOIifUC-vv_tj1Vw")
+model = genai.GenerativeModel("gemini-2.0-flash")
 app = Flask(__name__)
-
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
-
 @app.route("/gemini",methods=["GET","POST"])
 def gemini():
     return(render_template("gemini.html"))
-
 @app.route("/gemini_reply",methods=["GET","POST"])
 def gemini_reply():
     q = request.form.get("q")
     print(q)
-    #gemini
-    r=q
-
-    return(render_template("gemini_reply.html",r=r))
-
+    r = model.generate_content(q)
+    return(render_template("gemini_reply.html",r=r.text))
 if __name__ == "__main__":
     app.run()
-    
